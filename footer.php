d91f2dbd7efb8be4084d819fba3d1a5de5924175
agungribowo <ribowoagung@gmail.com>
2024-08-11 23:50:07 +0700

    beranda

---
+++ footer.php
@@ -0,0 +1,106 @@
+<!-- Footer START -->
+<div class="footer">
+    <div class="container p-sm-0">
+        <div class="row justify-content-between">
+            <div class="col-12 col-md-6 col-lg-4 col-xl-3">
+                <a href="index.html">
+                    <img class="img-fluid" src="assets/img/logo-footer.png" alt="logo">
+                </a>
+                <p class="global__desc footer__desc">sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore et dolore sed do
+                    eiusmod.</p>
+                <div class="footer__social">
+                    <ul class="footer__social-ul">
+                        <li>
+                            <a class="footer__social-link" href="https://www.facebook.com/"><i
+                                    class="fa-brands fa-facebook"></i></a>
+                        </li>
+                        <li>
+                            <a class="footer__social-link" href="https://linkedin.com"><i
+                                    class="fa-brands fa-linkedin-in"></i></a>
+                        </li>
+                        <li>
+                            <a class="footer__social-link" href="https://www.instagram.com"><i
+                                    class="fa-brands fa-instagram"></i></a>
+                        </li>
+                        <li>
+                            <a class="footer__social-link" href="https://twitter.com"><i
+                                    class="fa-brands fa-twitter"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4 col-xl-2 d-md-flex justify-content-center my-5 my-md-0">
+                <div class="">
+                    <h4 class="footer__sub">explore</h4>
+                    <ul class="footer__list">
+                        <li>
+                            <a class="footer__list-item" href="about.html">
+                                about us
+                            </a>
+                        </li>
+                        <li>
+                            <a class="footer__list-item" href="home-1.html#">
+                                pages
+                            </a>
+                        </li>
+                        <li>
+                            <a class="footer__list-item" href="blog.html">
+                                blog
+                            </a>
+                        </li>
+                        <li>
+                            <a class="footer__list-item" href="faqs.html">
+                                Privecy & Policy
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4 col-xl-2 mb-5 mb-md-0 mt-md-5 mt-lg-0">
+                <div class="">
+                    <h4 class="footer__sub">Donate</h4>
+                    <ul class="footer__list">
+                        <li>
+                            <a class="footer__list-item" href="donate.html">
+                                Give Donation
+                            </a>
+                        </li>
+                        <li>
+                            <a class="footer__list-item" href="volunteer.html">
+                                Become Volunteer
+                            </a>
+                        </li>
+                        <li>
+                            <a class="footer__list-item" href="home-1.html#">
+                                Child Sponsorship
+                            </a>
+                        </li>
+                        <li>
+                            <a class="footer__list-item" href="donate.html">
+                                Other Ways to Give
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-xl-4 mt-md-5 mt-xl-0">
+                <h4 class="footer__sub">Subscribe Newsletter</h4>
+                <p class="global__desc footer__desc">Join our newsletter to stay updated about our latest news and
+                    articles.
+                </p>
+                <form action="home-1.html#" method="POST" class="d-flex position-relative">
+                    <input class="footer__inp form-control" type="text" placeholder="Enter your email">
+                    <button class="global__btn footer__inp-btn">subscribe</button>
+                </form>
+            </div>
+        </div>
+        <hr class="footer__border">
+        <div class="row">
+            <div class="col-12 text-center">
+                <p class="footer__bottom">Copyright © 2023 Company Name and Address here</p>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Footer END -->
---
+++ js.php
@@ -0,0 +1,18 @@
+<!-- Jquary -->
+<script src="assets/js/jquery-1.12.4.min.js"></script>
+<!-- Venobox -->
+<script src="assets/js/venobox.min.js"></script>
+<!-- Counter JS -->
+<script src="assets/js/waypoints.min.js"></script>
+<script src="assets/js/jquery.counterup.min.js"></script>
+<!-- Slick JS -->
+<script src="assets/js/slick.min.js"></script>
+<!-- Bootstrap JS -->
+<script src="assets/js/bootstrap.bundle.min.js"></script>
+<!-- Aos JS -->
+<script src="assets/js/aos.js"></script>
+<!-- JS -->
+<script src="assets/js/app.js"></script>
+</body>
+
+</html>
